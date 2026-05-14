8aa18d207835f821184e8ded2d92d44333e11fd3
Prajapatipushkar <kppusakar2018@gmail.com>
2026-05-14 21:44:04 +0530

    Adding Activity project with mongoDB connection successfully

---
+++ activityservice/src/main/java/com/fitness/activityservice/controller/ActivityController.java
@@ -0,0 +1,17 @@
+package com.fitness.activityservice.controller;
+
+import com.fitness.activityservice.service.ActivityService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/activities")
+@AllArgsConstructor
+public class ActivityController {
+    private ActivityService activityService;
+
+
+}
